87821921c458c414f86d95903f68ef30a647e90c
JyotirmayaTembhurne <84905370+JyotirmayaTembhurne@users.noreply.github.com>
2023-09-07 14:05:57 +0530

    Update CreateHashmap.py

--- CreateHashmap.py
+++ CreateHashmap.py
@@ -1,5 +1,49 @@
-hashmap = dict()
-hashmap[1] = "this"
-hashmap[2] = "that"
-hashmap.pop(1)
-print(hashmap)
+class MyHashMap:
+    def __init__(self):
+        self.hashmap = dict()
+
+    def put(self, key: int, value: int) -> None:
+        self.hashmap[key] = value
+
+    def get(self, key: int) -> int:
+        if key in self.hashmap:
+            return self.hashmap[key]
+        return -1
+
+    def remove(self, key: int) -> None:
+        if key in self.hashmap:
+            self.hashmap.pop(key)
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
+
+
+class MyHashMap:
+    def __init__(self):
+        self.hashmap = dict()
+
+    def put(self, key: int, value: int) -> None:
+        self.hashmap[key] = value
+
+    def get(self, key: int) -> int:
+        try:
+            return self.hashmap[key]
+        except Exception:
+            return -1
+
+    def remove(self, key: int) -> None:
+        try:
+            self.hashmap.pop(key)
+        except Exception:
+            pass
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
